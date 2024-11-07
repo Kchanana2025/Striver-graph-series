111bfebc4716a49519040a35aa311bc48768f718
Kchanana2025 <kanikachanana2025@gmail.com>
2024-11-07 12:06:46 +0530

    ...

---
+++ enclaves/multisource_bfs_sol.cpp
@@ -0,0 +1,7 @@
+// Multisource BFS (Breadth-First Search) is an extension of the traditional Breadth-First Search algorithm used to find the
+// shortest path or minimum distance from multiple starting nodes (or sources) to other nodes in a graph, grid, or network.
+// Instead of initializing the BFS from a single source node, multisource BFS initializes the search from multiple nodes simultaneously.
+// Applications
+// Shortest distance in grids: Find the minimum steps from multiple starting points to other points in a grid (e.g., the nearest water source for each land cell).
+// Flood fill with multiple sources: Used in scenarios where you want to fill regions from multiple starting points simultaneously.
+// Social networks: Finding distances in a network graph, like the minimum "friendship path" from multiple influencers to other users.
